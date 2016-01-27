1113c74c8d4c512e52f54b56b040b7ac336676c0
Alan-Solitar <alsolita1@gmail.com>
2016-01-26 22:57:28 -0500

    Created Constants file
    -This file will hold all constants
    -Added Format as inner class

---
+++ app/src/main/java/com/burstlinker/budget/Constants.java
@@ -0,0 +1,14 @@
+package com.burstlinker.budget;
+
+/**
+ * Created by Alan Solitar on 2016/01/25.
+ */
+public class Constants
+{
+  public class Format
+  {
+      public static final String DATE_FORMAT = "yyyy-MM-dd";
+      public static final String DOLLAR_FORMAT = "$0.00";
+  }
+
+}
