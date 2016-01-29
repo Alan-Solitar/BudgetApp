3900bdbc041fb4a5304f4284a5ff8d0ba8978de0
Alan-Solitar <alsolita1@gmail.com>
2016-01-29 00:10:18 -0500

    Various changes to constants, strings, and styles
    -Added new money format in constants
    -Added a few new string resources
    -Added a new style for the entire app.

--- app/src/main/java/com/burstlinker/budget/Constants.java
+++ app/src/main/java/com/burstlinker/budget/Constants.java
@@ -9,6 +9,7 @@ public class Constants
   {
       public static final String DATE_FORMAT = "yyyy-MM-dd";
       public static final String DOLLAR_FORMAT = "$0.00";
+      public static final String DOLLARS_ONLY = "$0";
   }
 
 }
