a95c23466dc72fcea31f8ceb5887d848900e5c56
Alan-Solitar <alsolita1@gmail.com>
2016-01-30 15:37:09 -0500

    Converted this activity to a fragment

---
+++ app/src/main/java/com/burstlinker/budget/HistoryFrag.java
@@ -0,0 +1,56 @@
+package com.burstlinker.budget;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryFrag extends Fragment
+{
+    private ArrayList<Purchase> purchases=null;
+    private MyAdapter adapter;
+    //private DBHandler db;
+    private PurchaseHandler purchaseHandler;
+    private RecyclerView recycle;
+    private RecyclerView.LayoutManager myLayoutManager;
+    private Context context;
+    private View view;
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        context = getActivity();
+        purchaseHandler = new PurchaseHandler(getActivity());
+        purchases = purchaseHandler.getRecords(PurchaseHandler.LIMIT.NO_LIMIT);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.activity_data_display,container,false);
+        recycle = (RecyclerView) view.findViewById(R.id.my_recycler_view);
+        myLayoutManager = new LinearLayoutManager(context);
+        recycle.setLayoutManager(myLayoutManager);
+        adapter = new MyAdapter(purchases,context,this.getActivity().getSupportFragmentManager());
+        recycle.setAdapter(adapter);
+        recycle.addItemDecoration(new DividerItemDecoration(context));
+        return view;
+
+    }
+
+
+
+
+}
