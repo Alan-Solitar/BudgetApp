4efaa6337a66aad3ee971ad1726792d4323ad035
Alan-Solitar <alsolita1@gmail.com>
2016-02-05 22:41:03 -0500

    Implemented Hamburger icon for navigation drawer.

--- app/src/main/java/com/burstlinker/budget/MainActivity.java
+++ app/src/main/java/com/burstlinker/budget/MainActivity.java
@@ -1,10 +1,12 @@
 package com.burstlinker.budget;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -22,20 +24,41 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
     private Toolbar toolbar = null;
     private ViewPager pager =null;
     private TabLayout tabLayout=null;
-    List<Fragment> fragList= null;
-    Fragment currentFrag=null;
-    CustomPageAdapter pagerAdapter=null;
+    private List<Fragment> fragList= null;
+    private Fragment currentFrag=null;
+    private CustomPageAdapter pagerAdapter=null;
+    private NavigationView navigationView= null;
+    private DrawerLayout drawerLayout=null;
+    private ActionBarDrawerToggle actionBarDrawerToggle=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        tabs = getResources().getStringArray(R.array.tab_names);
         setContentView(R.layout.main_activity);
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        pager = (ViewPager) findViewById(R.id.pager);
 
+        //initialize variables
+        tabs = getResources().getStringArray(R.array.tab_names);
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        pager = (ViewPager) findViewById(R.id.pager);
         tabLayout= (TabLayout) findViewById(R.id.tabs);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //set custom toolbar
+        setSupportActionBar(toolbar);
+
+        //Code to show hamburger icon
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.drawer_open_text,
+                R.string.drawer_close_text);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+
+        //Add fragments to be given to the pager adapter
         fragList = new ArrayList<>();
         fragList.add(new EnterPurchaseFragment());
         fragList.add(new QuickFrag());
@@ -46,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
         pager.setAdapter(pagerAdapter);
         tabLayout.setTabsFromPagerAdapter(pagerAdapter);
         tabLayout.setupWithViewPager(pager);
-
 /*
 
         QuickFrag quickFrag = new QuickFrag();
@@ -54,6 +76,14 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
         transaction.add(R.id.container, quickFrag, "qfrag");
         transaction.commit();
 */
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+
     }
 
     //callback methods for fragments
